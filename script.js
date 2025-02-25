541e530bada1c7692fd934a60e121aeac84caae2
nguyentienthanh2209 <babysharkproacclome@gmail.com>
2025-02-25 07:30:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function login()
 (
-    document.getElementbyid(compiler
     let u = tên đăng nhập.value
     let p = mật khẩu.value
     if (u=="tienthanh" && p="123)
     {
         window.open()
+        document.getElementbyid(compiler).style.display="block";
     }
-}
+    else
+        document.write("sai tên đăng nhập hoặc mật khẩu")
